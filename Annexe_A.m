660f8cefc9bc27a4a034b7bc6a69104a7aa1e160
Giuseppe-Joey <giuseppe.lomonaco@usherbrooke.ca>
2023-10-10 12:11:37 -0400

    initial commit

---
+++ Annexe_A.m
@@ -0,0 +1,16 @@
+%%  S5 - APP4 - PROBLEMATIQUE - ANNEXE_A.M
+%   Auteur:     Giuseppe Lomonaco
+%   CIP:        LOMG2301
+%   Auteur:     Isabelle Handfield
+%   CIP:        HANI1401
+
+%   Date de creation:                       10-Octobre-2023
+%   Date de derniere modification:          10-Octobre-2023
+
+%   DESCRIPTION: fichier fourni en Annexe pour la problématique
+
+clc
+close all
+clear all
+
+
